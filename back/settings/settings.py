7d3b3f520362f494aa1e92057356783f425105b3
hhhhhhdong <gusehd520@gmail.com>
2022-06-18 02:27:30 +0900

    Add: 회원가입 기능 구현

--- back/settings/settings.py
+++ back/settings/settings.py
@@ -131,3 +131,14 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES' : (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
